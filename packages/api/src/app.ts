5959c05ba67ea433d5184fb18b950626a929e244
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-02-02 19:42:19 +0000

    Added Not Found route

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,10 +1,11 @@
 import compression from "compression";
 import cors from "cors";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
 import morgan from "morgan";
 import config from "./config";
+import utils from "./utils";
 
 const app: Application = express(); // Create Express Application
 
@@ -16,5 +17,9 @@ app.use(express.json({ limit: "15mb" })); // Enable JSON Parser with 15mb limit 
 app.use(morgan("dev")); // Enable Morgan for logging
 
 // app.use("/api/v1", routes);
+
+app.use((_req: Request, res: Response) => {
+	utils.response.send(res, utils.http.StatusNotFound, "Not Found", null);
+});
 
 export default app;
