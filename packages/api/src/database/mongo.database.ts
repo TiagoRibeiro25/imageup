3c91bd24638036d64c5850e7a038dcb5b6321d51
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-02-02 19:15:13 +0000

    Added Mongoose Schema

--- packages/api/src/database/mongo.database.ts
+++ packages/api/src/database/mongo.database.ts
@@ -1,15 +1,13 @@
 import mongoose, { Mongoose } from "mongoose";
 import config from "../config";
-// import MessageModel from '../models/Message.model';
-// import RoomModel from '../models/Room.model';
+import ImageModel from "../models/Image.model";
 
 const mongo = {
 	mongoose,
 	connect: (): Promise<Mongoose> => {
 		return mongoose.connect(process.env.MONGOOSE_URL, config.mongoose);
 	},
-	// Room: RoomModel,
-	// Message: MessageModel,
+	Image: ImageModel,
 };
 
 export default mongo;
---
+++ packages/api/src/models/Image.model.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const ImageSchema = new mongoose.Schema({
+	cloudinary_id: {
+		type: String,
+		required: true,
+	},
+	uploaded_at: {
+		type: Date,
+		default: Date.now,
+	},
+	date_to_delete: {
+		type: Date,
+		default: new Date(new Date().setDate(new Date().getDate() + 1)), // 1 day from now
+	},
+});
+
+export default mongoose.model("Image", ImageSchema);
