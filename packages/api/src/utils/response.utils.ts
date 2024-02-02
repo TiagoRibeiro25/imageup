49d4ee7e184f6d808b2530ec9e0035a067343f86
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-02-02 20:09:58 +0000

    Added utility function to handle 500 responses

--- packages/api/src/utils/response.utils.ts
+++ packages/api/src/utils/response.utils.ts
@@ -1,4 +1,6 @@
+import { log } from "console";
 import { Response } from "express";
+import httpUtils from "./http.utils";
 
 /**
  * Send a response to the client.
@@ -17,4 +19,21 @@ function send(response: Response, statusCode: number, message: string, data: any
 	response.end();
 }
 
-export default { send };
+/**
+ * Handles an error by sending a response with a 500 status code and logging the error details.
+ * @param response - The response object to send the error response.
+ * @param error - The error object that occurred.
+ * @param fileAbsolutePath - The absolute path of the file where the error occurred.
+ */
+function handleError(response: Response, error: Error, fileAbsolutePath: string) {
+	// Send the response with a 500 status code
+	send(response, httpUtils.StatusInternalServerError, "Something went wrong!", null);
+
+	// Get the file path (relative to the src folder)
+	const filePath = fileAbsolutePath.split("/src/").at(-1);
+
+	log(`[${filePath}] Error: ${error.name}`);
+	log(`[${filePath}] Message: ${error.message}`);
+}
+
+export default { send, handleError };
