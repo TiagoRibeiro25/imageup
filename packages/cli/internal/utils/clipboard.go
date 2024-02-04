814de494d8f60100bf0477d6408e7e6965b6c8a5
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-02-04 16:25:59 +0000

    Added TODO

--- packages/cli/internal/utils/clipboard.go
+++ packages/cli/internal/utils/clipboard.go
@@ -7,6 +7,8 @@ import (
 	"github.com/atotto/clipboard"
 )
 
+// TODO: Improve the CopyToClipboard function to work with more environments.
+
 // Copy copies the given text to the clipboard.
 // It uses the clipboard.WriteAll function to write the text to the clipboard.
 // If an error occurs while copying, it prints an error message and exits with a non-zero status code.
