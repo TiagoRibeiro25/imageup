0d41bb1bbbdeefc1a93c63b5074d2bc4a8781f4b
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-02-04 16:18:21 +0000

    Added utility function to copy text to the clipboard

--- packages/cli/go.mod
+++ packages/cli/go.mod
@@ -1,3 +1,5 @@
 module github.com/TiagoRibeiro25/imageup
 
 go 1.21.6
+
+require github.com/atotto/clipboard v0.1.4 // indirect
---
+++ packages/cli/internal/utils/clipboard.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/atotto/clipboard"
+)
+
+// Copy copies the given text to the clipboard.
+// It uses the clipboard.WriteAll function to write the text to the clipboard.
+// If an error occurs while copying, it prints an error message and exits with a non-zero status code.
+// After successful copying, it prints a success message.
+func CopyToClipboard(text string) {
+	err := clipboard.WriteAll(text)
+	if err != nil {
+		fmt.Println("Error copying URL to clipboard:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("URL copied to clipboard!")
+}
